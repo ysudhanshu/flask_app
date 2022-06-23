894eda3416a555735c7984fe23a50390bf57291a
Sudhanshu Kumar <ysudhanshu.kr@gmail.com>
2022-06-23 13:51:42 +0530

    Added app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return jsonify({
+        "msg": "App run successfull"
+    }), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
